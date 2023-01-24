07ede6d8b179d39ec5c2ba1685958ad5060c37d9
liuxing <578941470@qq.com>
2023-01-24 16:39:14 +0800

    隐藏用户敏感信息

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -17,7 +18,7 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
         // 如果存在，保存用户信息到ThreadLocal
-        UserHolder.saveUser((User) user);
+        UserHolder.saveUser((UserDTO) user);
         return true;
     }
 
