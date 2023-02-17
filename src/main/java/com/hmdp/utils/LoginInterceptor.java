ab185859bf52979384f70046487627481b18f89f
liuxing <578941470@qq.com>
2023-02-17 17:25:07 +0800

    登录拦截器优化

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,55 +1,26 @@
 package com.hmdp.utils;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.StrUtil;
-import com.hmdp.dto.UserDTO;
-import org.springframework.data.redis.core.StringRedisTemplate;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
+
 
 
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-    public StringRedisTemplate stringRedisTemplate;
-
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 获取请求头中的token，根据token获取redis中的用户
-        String token = request.getHeader("authorization");
-        if (StrUtil.isBlank(token)) {
+        // 判断是否需要拦截（threadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
             response.setStatus(401);
             return false;
         }
-        Map<Object, Object> userMap =
-                stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + token);
-
-        if (userMap.isEmpty()) {
-            response.setStatus(401);
-            return false;
-        }
-        // 将查询到的hash数据转为User对象
-        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false); // 三个参数分别是转换的map，要转化的User对象，是否忽略转换过程中的语法错误
-        // 如果存在，保存用户信息到ThreadLocal
-        UserHolder.saveUser(userDTO);
-        // 刷新token有效期
-        stringRedisTemplate.expire(LOGIN_USER_KEY + token, 30, TimeUnit.MINUTES);
-
         return true;
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
-    }
 }
---
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -0,0 +1,52 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
+
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    public StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 获取请求头中的token，根据token获取redis中的用户
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            return true;
+        }
+        Map<Object, Object> userMap =
+                stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + token);
+
+        if (userMap.isEmpty()) {
+            return true;
+        }
+        // 将查询到的hash数据转为User对象
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false); // 三个参数分别是转换的map，要转化的User对象，是否忽略转换过程中的语法错误
+        // 如果存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser(userDTO);
+        // 刷新token有效期
+        stringRedisTemplate.expire(LOGIN_USER_KEY + token, 30, TimeUnit.MINUTES);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+      UserHolder.removeUser();
+    }
+}
